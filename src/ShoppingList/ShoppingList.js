8ca4bd3f22efa3bcc5a62180be0a7b8d952799dc
tpartridge1 <tammy.partridge7@gmail.com>
2020-07-09 15:20:53 -0400

    added a new button and class

--- src/ShoppingList/ShoppingList.js
+++ src/ShoppingList/ShoppingList.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
+
 
 class ShoppingList extends Component {
 	 render() {
 	   return (     
        <div>
+         <h1>Make a list</h1>
+         <ul>
+           {this.props.items.map(items => (
+             <li>{items}</li>
+           ))}
+         </ul>
+         <form>
+           <input className= 'enter-itm' type='text' placeholder='enter item'></input>
+           <button className= 'enter-btn' type='submit'>Add</button>
+         </form>
      </div>
     );
   }
