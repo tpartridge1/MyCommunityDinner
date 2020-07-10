85bdb9a46779911c7a79ee8f832d6dc28e8c3a9a
tpartridge1 <tammy.partridge7@gmail.com>
2020-07-10 16:45:35 -0400

    added drop down menu

--- src/ShoppingList/ShoppingList.js
+++ src/ShoppingList/ShoppingList.js
@@ -1,19 +1,24 @@
 import React, {Component} from 'react';
-
+import './ShoppingList.css'
 
 class ShoppingList extends Component {
+
 	 render() {
 	   return (     
        <div>
          <h1>Make a list</h1>
-         <ul>
-           {this.props.items.map(items => (
-             <li>{items}</li>
-           ))}
-         </ul>
-         <form>
-           <input className= 'enter-itm' type='text' placeholder='enter item'></input>
-           <button className= 'enter-btn' type='submit'>Add</button>
+         <form onSubmit={this.addItem}>
+          <input id='add-listItem' placeholder='enter item' />
+          <select name="unit" id="unit">
+            <option value="ea">Each</option>
+            <option value="pkg">Package</option>
+            <option value="pound">lb</option>
+            <option value="ounces">oz</option>
+            <option value="bag">Bag</option>
+            <option value="cup">Cup</option>
+            <option value="box">Box</option>
+            </select>
+          <button className= 'enter-btn' type='submit'>Add</button>
          </form>
      </div>
     );
