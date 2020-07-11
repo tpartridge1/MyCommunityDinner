2c362d085c256198388950feb106950271615de7
tpartridge1 <tammy.partridge7@gmail.com>
2020-07-11 16:15:02 -0400

    added input focus, addItem function

--- src/App.js
+++ src/App.js
@@ -14,18 +14,54 @@ import 'tachyons';
 class App extends Component {
     constructor(props) {
       super(props);
-     this.state = {
-        items: []
+      this.state = {
+        items: [], //items input
+
      };
 
-      this.addItem = this.addItem.bind(this);
+      this.addItem = this.addItem.bind(this); //bind calls an item and waits
+      //this.setNewListItem = this.setNewListItem.bind(this); //bind adds the new item
           
     }
 
+    //itemValue(e) { 
+    //  this.setState({    //setState laods item value to memory (event)
+   //     items: e.target.value  
+    //  });
+    //}
+
+    //setNewListItem(e) {    //setState accepts new item and sets to memory 
+   //   this.setState({
+    //    newItem: this.state.items
+   //   });
+
+    //  this._input.focus();   //places a focus surrounding input text area
+   //   this._input.value = "";
+
+      //e.preventDefault(); //clear the input when submitting
+    //}
+
     addItem(e) {
-
+      if (this._inputElement.value !== "") {
+        var newItem = {
+          text: this._inputElement.value,
+          key: Date.now()
+        };
+     
+        this.setState((prevState) => {
+          return { 
+            items: prevState.items.concat(newItem) 
+          };
+        });
+       
+        this._inputElement.value = "";
+      }
+       
+      console.log(this.state.items);
+         
+      e.preventDefault();
     }
-
+    
 
   
     
