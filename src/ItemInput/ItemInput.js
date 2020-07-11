e261138e745b0012a676975c8a238e00bdc653f7
tpartridge1 <tammy.partridge7@gmail.com>
2020-07-11 16:15:54 -0400

    deleted div content

--- src/ItemInput/ItemInput.js
+++ src/ItemInput/ItemInput.js
@@ -4,13 +4,7 @@ class ItemInput extends Component {
   render() {
     return (    
       <div>
-      <form>
-      <h1>My Shopping List </h1>
-      <p>Enter items: </p>
-      <input
-        type="text" onChange={this.myChangeHandler}
-      />
-    </form>
+     
     </div>
    );
  }
