085af4eedad4657f7f817c4b2b34ca9a72a135eb
tpartridge1 <tammy.partridge7@gmail.com>
2020-07-15 14:59:33 -0400

    created delete items in child component, set to communicate with parent ShoppingList component

--- src/ItemInput/ItemInput.js
+++ src/ItemInput/ItemInput.js
@@ -1,16 +1,27 @@
 import React, {Component} from 'react';
 
+
+
 class ItemInput extends Component {
 
-  
+  createTasks(item) {
+    return <li key={item.key}>{item.text}</li>
+  }
+
   render() {
+    var shoppingEntries = this.props.entries;
+    var listItems = shoppingEntries.map(this.createTasks);
+  
     return (    
       <div>
+        <ul className="theList">
+          {listItems}
+      </ul>
      
     </div>
    );
  }
-}
+};
 
 
 export default ItemInput;
