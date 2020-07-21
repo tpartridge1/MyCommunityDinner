e9c772db9109e4a76058c927e966088c2bb3a6ab
tpartridge1 <tammy.partridge7@gmail.com>
2020-07-21 14:57:26 -0400

    updating files

--- src/PriceInput/PriceInput.js
+++ src/PriceInput/PriceInput.js
@@ -5,12 +5,10 @@ const PriceInput = props => {
     return (    
       <div>
       <form>
-      <p>Search Price: </p>
-      <input type="search" placeholder="search item price" onChange={e => console.log(e.target.value)}
+      <p>Enter Price: </p>
+      <input type="text" placeholder="enter item price" onChange={e => console.log(e.target.value)}
       />
-      <button>Submit</button>
-      <p>Enter price: </p>
-      <input type="currency" placeholder="enter item price" />
+      <button>Add</button>
     </form>
     </div>
    );
