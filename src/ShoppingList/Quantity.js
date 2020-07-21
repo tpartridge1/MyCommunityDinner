fd01e0a06e15d38dba88ee990745008dff8f4eda
tpartridge1 <tammy.partridge7@gmail.com>
2020-07-21 14:58:34 -0400

    initial commit

---
+++ src/ShoppingList/Quantity.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Quantity() {
+    return(
+        <div>
+         <p>Quantity: </p>
+         <input type='number'></input>     
+        </div>
+    );
+}
+
+
+export default Quantity;
